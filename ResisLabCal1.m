3bc054d7367d0d9d88ec4ca2c601dfb33e27789e
Soichi Tamashiro <wyvernys@live.com>
2017-04-20 12:19:54 -0500

    Update ResisLabCal1.m

--- ResisLabCal1.m
+++ ResisLabCal1.m
@@ -8,10 +8,12 @@
 % Asignatura : Resistencia de Materiales y Calculo de Elementos Finitos
 %
 % 1.-
-% use deg2rad(teta)
+% Use deg2rad(teta) para cambiar de degre a radianes
+% Verifica la escritura de la ecuación
 syms x xpi
 F = sqrt((x^(1/3)/(sec(x)+cot(x)))+sqrt((sqrt(exp(x))+1)/(1+xpi^3)));
 pretty (F)
+% Opera la ecuación
 f = sqrt((3^(1/3)/(sec(deg2rad(17))+cot(deg2rad(75))))+sqrt((sqrt(exp(1))+1)/(1+pi^3)))
 
    
